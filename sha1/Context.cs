47d526e96220af78a35109751a22f400adc9b9ac
Arek Wnukowski <arek.wnukowski@1fins.com>
2017-06-23 09:17:17 +0100

    initial implementation

---
+++ sha1/Context.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Sha1
+{
+    public class Context
+    {
+        private static readonly string _currentProcessName = Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+
+        public string ExeName { get { return _currentProcessName; } }
+        public string HashName { get; private set; }
+        public HashAlgorithm HashAlgorithm { get; private set; }
+        private Context()
+        {
+        }
+
+        public static Context FromCurrentProcess()
+        {
+            if (_currentProcessName.ToLower() == "md5.exe")
+            {
+                return new Context
+                {
+                    HashName = "MD5",
+                    HashAlgorithm = new MD5CryptoServiceProvider()
+                };
+            }
+            else if (_currentProcessName.ToLower() == "sha512.exe")
+            {
+                return new Context
+                {
+                    HashName = "SHA-512",
+                    HashAlgorithm = new SHA512CryptoServiceProvider()
+                };
+            }
+            else if (_currentProcessName.ToLower() == "sha256.exe")
+            {
+                return new Context
+                {
+                    HashName = "SHA-256",
+                    HashAlgorithm = new SHA256CryptoServiceProvider()
+                };
+            }
+            else if (_currentProcessName.ToLower() == "sha1.exe")
+            {
+                return new Context
+                {
+                    HashName = "SHA-1",
+                    HashAlgorithm = new SHA1CryptoServiceProvider()
+                };
+            }
+            else
+            {
+                throw new InvalidOperationException("Executable name must be named one of the following: \"md5.exe\", \"sha512.exe\", \"sha256.exe\" or \"sha1.exe\".");
+            }
+        }
+    }
+}
---
+++ sha1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Sha1
+{
+    class Program
+    {
+        private static readonly Context _context = Context.FromCurrentProcess();
+
+        static int Main(string[] args)
+        {
+            if (args.Length == 0) {
+                Usage();
+                return 1;
+            }
+
+            try
+            {
+                var hash = HashFile(args[0]);
+
+                StdOut(hash);
+
+                return 0;
+            }
+            catch (Exception exception)
+            {
+                Console.Error.WriteLine(exception.Message);
+                return 2;
+            }
+        }
+
+        private static byte[] HashFile(string path)
+        {
+            using (var stream = File.OpenRead(path))
+            {
+                return _context.HashAlgorithm.ComputeHash(stream);
+            }
+        }
+
+        private static void StdOut(byte[] bytes)
+        {
+            var builder = new StringBuilder();
+
+            foreach (byte b in bytes)
+            {
+                builder.Append(b.ToString("X2"));
+            }
+
+            Console.WriteLine(builder.ToString().ToLower());
+        }
+
+        private static void Usage()
+        {
+            Console.WriteLine($"Usage: {_context.ExeName} file");
+            Console.WriteLine();
+            Console.WriteLine($"Compute {_context.HashName} hash of file and output to STDOUT its hex string representation.");
+        }
+    }
+}
